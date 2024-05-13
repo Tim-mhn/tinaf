c99d8c32047def2922b7cacf6eb562bccc85ccb5
Tim Meehan <tim.meehan097@gmail.com>
2024-05-13 17:35:14 +0200

    [create-tinaf] used prompt  to customize project name + improve success messages and indications

--- packages/create-tinaf/src/main.ts
+++ packages/create-tinaf/src/main.ts
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { randomUUID } from 'crypto';
+import prompts from 'prompts';
 
 function copy(src: string, dest: string) {
   const stat = fs.statSync(src);
@@ -22,10 +22,9 @@ function copyDir(srcDir: string, destDir: string) {
 }
 
 const TEMPLATE_DIR = 'demo';
-const targetDir = `demo-${randomUUID()}`;
 const cwd = process.cwd();
 
-function init() {
+function init(targetDir: string = 'tinaf-start-app') {
   const templateDir = path.resolve(
     fileURLToPath(import.meta.url),
     '../../',
@@ -50,10 +49,23 @@ function init() {
 }
 
 try {
-  init();
-  console.info('Tinaf starter project successfully 🎉🎉🎉');
-  console.info('Run npm install or yarn install to install dependencies');
-  console.info('Then run npm run dev or yarn dev to start the dev server');
+  (async () => {
+    const response = await prompts({
+      type: 'text',
+      name: 'projectName',
+      message: 'Give a name to the project:',
+    });
+
+    const { projectName } = response;
+
+    init(projectName);
+
+    console.info('Tinaf starter project successfully 🎉🎉🎉\b\n');
+    console.info('To get started, follow these 3 steps\b\n');
+    console.info('1. Open the directory: cd ', projectName);
+    console.info('2. Install dependencies: npm install or yarn install');
+    console.info('3. Start the dev server: npm run dev or yarn dev');
+  })();
 } catch (err) {
   console.error(err);
 }
