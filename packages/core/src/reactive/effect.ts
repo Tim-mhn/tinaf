19993cca9cb179b0243e94e371c07acfb0797467
Tim Meehan <tim.meehan097@gmail.com>
2024-08-14 11:43:04 +0200

    test gh

--- packages/core/src/reactive/effect.ts
+++ packages/core/src/reactive/effect.ts
@@ -1,6 +1,8 @@
 import type { ReactiveValue } from './reactive';
 import { watchAllSources } from './watch';
 
+console.log('foo');
+
 export function effect<T>(callbackFn: () => void, sources: ReactiveValue<T>[]) {
   const sub = watchAllSources(sources).subscribe(() => callbackFn());
 
